2763ad96cdd2f00d148679ea95ca2920e20f3f32
Xiao Shen <xiao.shen@capco.com>
2019-04-10 00:23:51 -0400

    commit custom airflow config file

--- dags/test_operators.py
+++ dags/test_operators.py
@@ -2,11 +2,13 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators import MyFirstOperator, MyFirstSensor
-
+from airflow.contrib.sensors.sftp_sensor import SFTPSensor
 
 dag = DAG('my_test_dag', description='Another tutorial DAG',
           schedule_interval='0 12 * * *',
           start_date=datetime(2017, 3, 20), catchup=False)
+
+gcp_sftp_check= SFTPSensor(task_id='sftp_sensor_task', path='/home/xshn/blockspring-results.csv', sftp_conn_id='GCP_SFTP')
 
 dummy_task = DummyOperator(task_id='dummy_task', dag=dag)
 
@@ -15,4 +17,4 @@ sensor_task = MyFirstSensor(task_id='my_sensor_task', poke_interval=30, dag=dag)
 operator_task = MyFirstOperator(my_operator_param='This is a test.',
                                 task_id='my_first_operator_task', dag=dag)
 
-dummy_task >> sensor_task >> operator_task
+dummy_task >> gcp_sftp_check >> sensor_task >> operator_task 
