6b64386f1ff1630f851d869ccdf0e8a2d6d08a7e
Xiao Shen <xiaoshen2009@gmail.com>
2019-04-11 14:11:39 -0400

    run urgent and regular in parallel

--- dags/test_operators.py
+++ dags/test_operators.py
@@ -30,11 +30,15 @@ sensor_task = MyFirstSensor(task_id='my_sensor_task', poke_interval=30, dag=dag)
 operator_task = MyFirstOperator(my_operator_param='This is a test.',
                                 task_id='my_first_operator_task', dag=dag, execution_timeout=timedelta(minutes=30))
 
-ftp_get_file_sensor_task = FTPGetFileSensor(task_id='sftp_get_file_sensor', ssh_conn_id='COOP_SFTP_PROD', extract_file_type='regular', poke_interval=30, dag=dag)
+ftp_get_regular_file_sensor_task = FTPGetFileSensor(task_id='get_regular_file_sftpsensor', ssh_conn_id='COOP_SFTP_PROD', extract_file_type='regular', poke_interval=30, dag=dag)
+
+ftp_get_urgent_file_sensor_task = FTPGetFileSensor(task_id='get_urgent_file_sftpsensor', ssh_conn_id='COOP_SFTP_PROD', extract_file_type='urgent', poke_interval=30, dag=dag)
 
 # sftp_test_file=SFTPOperator(task_id='test_sftp', ssh_conn_id='COOP_SFTP_PROD', local_filepath='./xiaoreceive.txt', remote_file_path='/xiao.txt', operation='GET', create_intermediate_dirs=True, dag=dag)
 put_test_file = SFTPOperator(task_id='test_sftp', ssh_conn_id='COOP_SFTP_PROD', local_filepath='./xiao3.txt', remote_filepath='/xiao.txt', operation='GET', dag=dag)
 sftp_sensor_file = SFTPSensor(task_id='sftp_sensor', sftp_conn_id='COOP_SFTP_PROD', path='/home/xshn/blockspring-resultsput.csv')
 # dummy_task >> put_test_file >> sftp_sensor_file >> sensor_task >>  operator_task 
-dummy_task >> ftp_get_file_sensor_task >> sensor_task >>  operator_task 
+# dummy_task >> ftp_get_file_sensor_task >> sensor_task >>  operator_task 
+
+dummy_task.set_upstream([ftp_get_regular_file_sensor_task, ftp_get_urgent_file_sensor_task])
 
