ec3dbe7e84bd9819d8d5de6e6730073a03057cd2
Xiao Shen <xiaoshen2009@gmail.com>
2019-04-15 12:13:16 -0400

    do not run the time task for now

--- dags/cpoc_client_conversion_dag.py
+++ dags/cpoc_client_conversion_dag.py
@@ -8,14 +8,14 @@ from airflow.contrib.operators.sftp_operator import SFTPOperator
 from airflow.utils.dates import days_ago
 
 import pytz
-tz = pytz.timezone('America/Toronto')
+# tz = pytz.timezone('America/Toronto')
 
-def localize_utc_tz(d):
-    return tz.fromutc(d)
+# def localize_utc_tz(d):
+#     return tz.fromutc(d)
 
 default_args= {
     'owner': 'CPOC',
-    'start_date': days_ago(1),
+    'start_date': days_ago(2),
     'depends_on_past': False,
     'email': ['xiaoshen2009@gmail.com', 'xiao_shen@cooperators.ca'],
     'email_on_failure': True,
@@ -25,18 +25,18 @@ default_args= {
     'retry_delay': timedelta(minutes=1)
 }
 
-dag = DAG('cpoc_client_conversion', description='Another tutorial DAG', schedule_interval='0 8 * * 1-5', start_date=days_ago(1), catchup=False, default_args = default_args,user_defined_filters={'localtz': localize_utc_tz,})
+dag = DAG('cpoc_client_conversion', description='Another tutorial DAG', schedule_interval='0 8 * * 1-5', start_date=days_ago(1), catchup=False, default_args = default_args)
 
 
 initiation_task = DummyOperator(task_id='initiation_task', dag=dag)
 
 cleanup_task = DummyOperator(task_id='cleanup_task', dag=dag)
 
-time_task = BashOperator(
-        task_id='task_for_testing_file_log_handler',
-        dag=dag,
-        bash_command='echo UTC {{ ts }}, Local {{ execution_date | localtz }} next {{ next_execution_date | localtz }}',
-)
+# time_task = BashOperator(
+#         task_id='task_for_testing_file_log_handler',
+#         dag=dag,
+#         bash_command='echo UTC {{ ts }}, Local {{ execution_date | localtz }} next {{ next_execution_date | localtz }}',
+# )
 
 ftp_get_regular_file_sensor_task = FTPGetFileSensor(task_id='get_regular_file_sftpsensor', ssh_conn_id='COOP_SFTP_PROD', regular_or_urgent='regular', extract_or_email='extract', poke_interval=30, dag=dag)
 
@@ -54,4 +54,4 @@ initiation_task.set_downstream([ftp_get_regular_file_sensor_task, ftp_get_urgent
 client_conversion_task.set_upstream([ftp_get_regular_file_sensor_task, ftp_get_regular_email_sensor_task])
 
 client_conversion_task >> cleanup_task
-initiation_task.set_upstream(time_task)
+# initiation_task.set_upstream(time_task)
